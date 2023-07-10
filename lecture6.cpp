3d05c99436efc2d00cd031480637abdbebb42d25
Abinash Patra <74194477+Abinash1510@users.noreply.github.com>
2023-07-10 21:59:12 +0530

    Update lecture6.cpp

--- lecture6.cpp
+++ lecture6.cpp
@@ -1,26 +1,34 @@
-// 72. Edit Distance
+#include <iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
 
-class Solution {
-public:
-    int minDistance(string word1, string word2) {
-        int n = word1.size(), m = word2.size();
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1));
+using namespace std;
 
-        for (int i = 0; i <= n; ++i) {
-            for (int j = 0; j <= m; ++j) {
-                if (!i || !j)
-                    dp[i][j] = i + j;
-                else if (word1[i - 1] == word2[j - 1])
-                    dp[i][j] = dp[i - 1][j - 1];
-                else
-                    dp[i][j] = 1 + min({
-                        dp[i][j - 1],    // insert
-                        dp[i - 1][j],    // delete
-                        dp[i - 1][j - 1] // replace
-                    });
+int editDistance(string a, string b) {
+    int m = a.size(), n = b.size();
+    vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+
+    for (int i = 0; i <=m; i++) {
+        for (int j = 0; j <= n; j++) {
+            if(i==0){
+                dp[i][j] = j;
+            }
+            else if(j==0){
+                dp[i][j] = i;
+            }
+            else if (a[i-1] == b[j-1]) {
+                dp[i][j] = dp[i-1][j-1] ;
+            }
+            else{
+                dp[i][j] = 1 + min(dp[i-1][j-1], min(dp[i][j-1], dp[i-1][j]));
             }
         }
-
-        return dp[n][m];
     }
-};
+
+    return dp[m][n];
+}
+
+int main(){
+
+}
