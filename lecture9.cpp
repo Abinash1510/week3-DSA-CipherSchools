40b42feef7145fc4151aaec8f324e72bfafbc67e
Abinash Patra <74194477+Abinash1510@users.noreply.github.com>
2023-07-10 22:16:28 +0530

    Update lecture9.cpp

--- lecture9.cpp
+++ lecture9.cpp
@@ -1,94 +1,48 @@
-// C++ implementation of search and insert
-// operations on Trie
-#include <bits/stdc++.h>
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+
+
 using namespace std;
+#define CHARSET 256
 
-const int ALPHABET_SIZE = 26;
-
-// trie node
-struct TrieNode
-{
-	struct TrieNode *children[ALPHABET_SIZE];
-
-	// isEndOfWord is true if the node represents
-	// end of a word
-	bool isEndOfWord;
+class TrieNode {
+    public:
+    bool isWord; // int val;
+    TrieNode *children[256];
+    TrieNode() {
+        this->isWord = false;
+        for (int i = 0; i < CHARSET; i++)
+        this->children[i] = NULL;
+    }
 };
 
-// Returns new trie node (initialized to NULLs)
-struct TrieNode *getNode(void)
-{
-	struct TrieNode *pNode = new TrieNode;
 
-	pNode->isEndOfWord = false;
 
-	for (int i = 0; i < ALPHABET_SIZE; i++)
-		pNode->children[i] = NULL;
+class Trie{
+    TrieNode *root;
+    public:
+    Trie() {
+       this->root = new TrieNode();
+    }
 
-	return pNode;
+    void Insert(const string &str) {
+        TrieNode* crawler = this->root;
+        for(int i=0; i<str.size(); i++){
+            if(crawler->children[int(str[i])] == NULL){
+                crawler->children[int(str[i])] == new TrieNode();
+            }
+            crawler = crawler->children[int(str[i])];
+        }
+        crawler->isWord = true;
+    }
+};
+
+
+
+
+int main(){
+    
 }
 
-// If not present, inserts key into trie
-// If the key is prefix of trie node, just
-// marks leaf node
-void insert(struct TrieNode *root, string key)
-{
-	struct TrieNode *pCrawl = root;
-
-	for (int i = 0; i < key.length(); i++)
-	{
-		int index = key[i] - 'a';
-		if (!pCrawl->children[index])
-			pCrawl->children[index] = getNode();
-
-		pCrawl = pCrawl->children[index];
-	}
-
-	// mark last node as leaf
-	pCrawl->isEndOfWord = true;
-}
-
-// Returns true if key presents in trie, else
-// false
-bool search(struct TrieNode *root, string key)
-{
-	struct TrieNode *pCrawl = root;
-
-	for (int i = 0; i < key.length(); i++)
-	{
-		int index = key[i] - 'a';
-		if (!pCrawl->children[index])
-			return false;
-
-		pCrawl = pCrawl->children[index];
-	}
-
-	return (pCrawl->isEndOfWord);
-}
-
-// Driver
-int main()
-{
-	// Input keys (use only 'a' through 'z'
-	// and lower case)
-	string keys[] = {"the", "a", "there",
-					"answer", "any", "by",
-					"bye", "their" };
-	int n = sizeof(keys)/sizeof(keys[0]);
-
-	struct TrieNode *root = getNode();
-
-	// Construct trie
-	for (int i = 0; i < n; i++)
-		insert(root, keys[i]);
-
-	// Search for different keys
-	char output[][32] = {"Not present in trie", "Present in trie"};
-
-	// Search for different keys
-	cout<<"the"<<" --- "<<output[search(root, "the")]<<endl;
-	cout<<"these"<<" --- "<<output[search(root, "these")]<<endl;
-	cout<<"their"<<" --- "<<output[search(root, "their")]<<endl;
-	cout<<"thaw"<<" --- "<<output[search(root, "thaw")]<<endl;
-	return 0;
-}
