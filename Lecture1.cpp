49a090dbe6c55430dfc49f39aaa11aec4eeabbef
Abinash Patra <74194477+Abinash1510@users.noreply.github.com>
2023-07-10 21:12:47 +0530

    Update Lecture1.cpp

--- Lecture1.cpp
+++ Lecture1.cpp
@@ -1,38 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
 
-// Coin Change -> Infinite Coins
-// Subset Sum -> Finite Coins
-// Rod Cutting
-// 01 - Knapsack
-// Edit Distance
-// Longest Increasing Subsequence
-// Longest Palindromic Substring/Subsequence
-// Longest Increasing Path in the Matrix
-
-
-// Coin Change -> Infinite Coins
-class Solution {
+class Solution
+{
 public:
-    int coinChange(vector<int>& coins, int amount) {
-        int N = coins.size();
-        vector<vector<int>> dp(N + 1, vector<int>(amount + 1, INT_MAX));
-
-        for (int i = 0; i <= N; i++) {
-            dp[i][0] = 0;
-        }
-
-        for (int s = 1; s <= amount; s++) {
-            for (int j = 1; j <= N; j++) {
-                if (s >= coins[j - 1]) {
-                    if (dp[j][s - coins[j - 1]] < INT_MAX) {
-                        dp[j][s] = min(1 + dp[j][s - coins[j - 1]], dp[j - 1][s]);
-                    }
-                }
-                else {
-                    dp[j][s] = dp[j - 1][s];
+    int coinChange(vector<int> &coins, int amount)
+    {
+        int size = coins.size();
+        vector<int> dp(amount + 1, INT_MAX);
+        dp[0] = 0;
+        for (int a = 1; a <= amount; a++)
+        {
+            for (int j = 1; j <= size; j++)
+            {
+                if (a >= coins[j - 1])
+                {
+                    dp[a] = min(dp[a], (dp[a - coins[j - 1]] == INT_MAX ? INT_MAX : 1 + dp[a - coins[j - 1]]));
                 }
             }
         }
 
-        return dp[N][amount] == INT_MAX ? -1 : dp[N][amount];
+        return dp[amount] == INT_MAX ? -1 : dp[amount];
     }
 };
+
+int main()
+{
+    return 0;
+}
